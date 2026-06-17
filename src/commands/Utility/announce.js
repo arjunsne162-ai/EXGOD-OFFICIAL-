105880a38a4982248021507c71b77d3f6fc9d043
arjunsne162-ai <arjunsne162@gmail.com>
2026-06-17 18:05:47 +0530

    Add announce command

---
+++ src/commands/Utility/announce.js
@@ -0,0 +1,55 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { createEmbed } from '../../utils/embeds.js';
+import { logger } from '../../utils/logger.js';
+import { handleInteractionError } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('announce')
+    .setDescription('Send an announcement')
+    .addStringOption(option =>
+      option
+        .setName('message')
+        .setDescription('Announcement message')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    try {
+      const message = interaction.options.getString('message');
+
+      const embed = createEmbed({
+        title: '📢 Announcement',
+        description: message
+      });
+
+      await interaction.channel.send({
+        embeds: [embed]
+      });
+
+      await InteractionHelper.safeReply(interaction, {
+        content: '✅ Announcement sent successfully!'
+      });
+
+      logger.info('Announcement command executed', {
+        userId: interaction.user.id,
+        guildId: interaction.guildId
+      });
+
+    } catch (error) {
+      logger.error('Announcement command execution failed', {
+        error: error.message,
+        stack: error.stack,
+        userId: interaction.user.id,
+        guildId: interaction.guildId
+      });
+
+      await handleInteractionError(interaction, error, {
+        commandName: 'announce',
+        source: 'announce_command'
+      });
+    }
+  }
+};
